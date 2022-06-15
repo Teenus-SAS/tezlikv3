65052ceb9d391dd5c0926ff9299dc66ed59b3d7a
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2022-06-15 09:22:07 -0500

    creacion estructura planeacion

--- app/controllers/templateController.php
+++ app/controllers/templateController.php
@@ -7,6 +7,11 @@ class templateController
         include __DIR__ . '../../views/templateOperations.php';
     }
 
+    public function ctrTemplatePlanning()
+    {
+        include __DIR__ . '../views/templatePlanning.php';
+    }
+
     public function ctrTemplateAdmin()
     {
         include __DIR__ . '../views/templateAdmin.php';
