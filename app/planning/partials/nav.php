b08b24671d415019285f81fa66436b257fd11518
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2022-06-16 12:29:49 -0500

    organizacion menus configuracion

--- app/planning/partials/nav.php
+++ app/planning/partials/nav.php
@@ -8,21 +8,16 @@
                             <i class="bx bxs-dashboard mr-1"></i> Dashboards
                             <i class="bx bx-chevron-down"></i>
                         </a>
-                        <!-- <div class="dropdown-menu" aria-labelledby="topnav-dashboard">
-                            <a class="dropdown-item" href="index.html"> Multi Purpose </a>
-                            <a class="dropdown-item" href="dashboard2.html"> E-commerce </a>
-                            <a class="dropdown-item" href="dashboard3.html"> Server Statistics </a>
-                        </div> -->
                     </li>
 
                     <li class="nav-item">
                         <a class="nav-link dropdown-toggle" href="#" onclick="loadContent('page-content','../app/views/analysis/prices.php')" id="topnav-dashboard" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="bx bxs-customize mr-1"></i> Generar Precios
+                            <i class="bx bxs-customize mr-1"></i> Programación
                             <i class="bx bx-chevron-down"></i>
                         </a>
                     </li>
 
-                    <li class="nav-item dropdown">
+                    <!-- <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" onclick="loadContent('page-content','../app/views/analysis/materials.php')" id="topnav-ui" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="bx bx-tone mr-1"></i> Análisis Materia Prima
                             <i class="bx bx-chevron-down"></i>
@@ -117,9 +112,6 @@
                                     <span> Tutoriales </span>
                                     <i class="bx bx-chevron-right"></i>
                                 </a>
-                                <!-- <div class="dropdown-menu" aria-labelledby="topnav-tables">
-                                    <a class="dropdown-item" href="table-basic.html"> Basic Table </a>
-                                </div> -->
                             </div>
                             <div class="dropdown">
                                 <a href="javascript:void(0);" onclick="loadContent('page-content','../app/views/support/emailSupport.php')" class="dropdown-item dropdown-toggle" aria-expanded="false">
@@ -127,80 +119,10 @@
                                     <span> Soporte</span>
                                     <i class="bx bx-chevron-right"></i>
                                 </a>
-                                <!-- <div class="dropdown-menu" aria-labelledby="topnav-Charts">
-                                    <a class="dropdown-item" href="charts-apex.html"> Apex </a>
-                                    <a class="dropdown-item" href="charts-c3.html"> C3 </a>
-                                    <a class="dropdown-item" href="charts-chartist.html">Chartist </a>
-                                    <a class="dropdown-item" href="charts-chartjs.html"> Chart JS </a>
-                                    <a class="dropdown-item" href="charts-flot.html"> Flot </a>
-                                    <a class="dropdown-item" href="charts-knob.html"> Knob </a>
-                                    <a class="dropdown-item" href="charts-morris.html"> Morris </a>
-                                    <a class="dropdown-item" href="charts-peity.html"> Peity </a>
-                                    <a class="dropdown-item" href="charts-sparklines.html"> Sparklines </a>
-                                </div> -->
-                            </div>
-                            <!-- <div class="dropdown">
-                                <a href="javascript:void(0);" class="dropdown-item dropdown-toggle" aria-expanded="false">
-                                    <i class="bx bx-aperture mr-1"></i>
-                                    <span> Icons</span>
-                                    <i class="bx bx-chevron-right"></i>
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="topnav-icons">
-                                    <a class="dropdown-item" href="icons-boxicons.html"> Box Icon </a>
-                                    <a class="dropdown-item" href="icons-feather.html"> Feather Icon </a>
-                                    <a class="dropdown-item" href="icons-mdi.html"> Material Design Icons </a>
-                                    <a class="dropdown-item" href="icons-simple-line.html"> Simple Line Icons </a>
-                                    <a class="dropdown-item" href="icons-themify.html"> Themify Icons </a>
-                                    <a class="dropdown-item" href="icons-two-tone.html"> Two Tone Icons </a>
-                                    <a class="dropdown-item" href="icons-font-awesome.html"> Font Awesome 5 </a>
-                                </div>
-                            </div> -->
-                            <!-- <div class="dropdown">
-                                <a href="javascript:void(0);" class="dropdown-item dropdown-toggle" aria-expanded="false">
-                                    <i class="bx bx-map mr-1"></i>
-                                    <span> Map </span>
-                                    <i class="bx bx-chevron-right"></i>
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="topnav-contact">
-                                    <a class="dropdown-item" href="maps-vector.html"> Vector Map </a>
-                                    <a class="dropdown-item" href="maps-google.html"> Google Map </a>
-                                </div>
-                            </div> -->
-                        </div>
-                    </li>
-                    <!-- <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="topnav-ui" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="bx bx-file mr-1"></i> Utility
-                            <i class="bx bx-chevron-down"></i>
-                        </a>
-                        <div class="dropdown-menu mega-dropdown-menu dropdown-mega-menu-xl dropdown-menu-right" aria-labelledby="topnav-utility">
-                            <div class="row">
-                                <div class="col-lg-4">
-                                    <a class="dropdown-item" href="utility-animation.html"> Animation </a>
-                                    <a class="dropdown-item" href="utility-activity.html"> Activity </a>
-                                    <a class="dropdown-item" href="utility-coming-soon.html"> Coming Soon </a>
-                                    <a class="dropdown-item" href="utility-faq.html"> FAQs </a>
-                                    <a class="dropdown-item" href="utility-fix-left.html"> Fix Left Sidebar </a>
-                                    <a class="dropdown-item" href="utility-fix-right.html"> Fix Right Sidebar </a>
-                                    <a class="dropdown-item" href="utility-gallery.html"> Gallery </a>
-                                </div>
-                                <div class="col-lg-4">
-                                    <a class="dropdown-item" href="utility-helperclasses.html"> Helper Classes </a>
-                                    <a class="dropdown-item" href="utility-lightbox.html"> Lightbox </a>
-                                    <a class="dropdown-item" href="utility-maintenance.html"> Maintenance </a>
-                                    <a class="dropdown-item" href="utility-pricing.html"> Pricing </a>
-                                    <a class="dropdown-item" href="utility-scrollbar.html"> Scrollbar </a>
-                                </div>
-                                <div class="col-lg-4">
-                                    <a class="dropdown-item" href="utility-search-result.html"> Search Result </a>
-                                    <a class="dropdown-item" href="utility-starterpage.html"> Starter Page </a>
-                                    <a class="dropdown-item" href="utility-timeline.html"> Timeline </a>
-                                    <a class="dropdown-item" href="utility-timeline-horizontal.html"> Timeline Horizontal </a>
-                                    <a class="dropdown-item" href="utility-treeview.html"> Tree View </a>
-                                </div>
                             </div>
                         </div>
                     </li> -->
+
                 </ul>
             </div>
         </nav>
