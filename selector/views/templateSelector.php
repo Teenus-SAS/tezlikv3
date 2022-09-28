170ee38a92641e3da5998e5c5a20d6c0e9264b32
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2022-09-28 12:00:14 -0500

    comentarias lineas de session

--- selector/views/templateSelector.php
+++ selector/views/templateSelector.php
@@ -1,10 +1,10 @@
 <?php
 
-use tezlikv3\dao\UserInactiveTimeDao;
+/* use tezlikv3\dao\UserInactiveTimeDao;
 
 require_once(dirname(dirname(__DIR__)) . "../api/src/dao/app/global/login/UserInactiveTimeDao.php");
 $userinactivetimeDao = new UserInactiveTimeDao();
-$userinactivetimeDao->findSession();
+$userinactivetimeDao->findSession(); */
 ?>
 
 <!DOCTYPE html>
