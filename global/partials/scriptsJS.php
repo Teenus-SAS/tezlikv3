d0b9e89fe534c47807ccdf1143350aede7ea3d35
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-02-23 17:36:58 -0500

    Ajustes vista movil

--- global/partials/scriptsJS.php
+++ global/partials/scriptsJS.php
@@ -18,7 +18,7 @@
 
 <!-- page JS -->
 <script src="/assets/js/app.js"></script>
-<!-- <script src="/global/js/global/sessionUser.js"></script> -->
+<script src="/global/js/global/sessionUser.js"></script>
 
 <!-- Datatables -->
 <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
