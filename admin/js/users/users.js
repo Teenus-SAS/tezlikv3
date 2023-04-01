cc145ff4b2d62e89de6a3f349864a0414cae8f82
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-04-01 17:08:45 -0500

    Ajustes usuarios

--- admin/js/users/users.js
+++ admin/js/users/users.js
@@ -190,7 +190,7 @@ $(document).ready(function () {
       $('#formCreateUser').trigger('reset');
       updateTable();
       toastr.success(data.message);
-      console.log(data.pass);
+      // console.log(data.pass);
       return false;
     } else if (data.error == true) toastr.error(data.message);
     else if (data.info == true) toastr.info(data.message);
