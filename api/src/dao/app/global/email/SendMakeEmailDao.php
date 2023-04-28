8fa2c6ba6228b6eb86e9866ae1a80d239743bd6d
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-04-28 18:05:39 -0500

    Ajustes generales

--- api/src/dao/app/global/email/SendMakeEmailDao.php
+++ api/src/dao/app/global/email/SendMakeEmailDao.php
@@ -35,15 +35,15 @@ class SendMakeEmailDao
     public function SendEmailPassword($email, $password)
     {
         // the message
-        $msg = 'Hola,\n
+        $msg = "Hola,\n
         Recientemente solicitó recordar su contraseña por lo que para mayor seguridad creamos una nueva. Para ingresar a Tezlik puede hacerlo con:\n
-        · Nombre de usuario:' . $email . '\n
-        · Contraseña:' . $password . '\n
+        · Nombre de usuario:  $email \n
+        · Contraseña: $password \n
         Las contraseñas generadas a través de nuestra plataforma son muy seguras solo se envían al correo electrónico del contacto de la cuenta.
         Si le preocupa la seguridad de la cuenta o sospecha que alguien está intentando obtener acceso no autorizado, puede estar 
         seguro que las contraseñas son generadas aleatoriamente, sin embargo, le recomendamos ingresar a la plataforma con la nueva clave y cambiarla por una nueva.
         Saludos,\n\n
-        Equipo de Soporte Tezlik';
+        Equipo de Soporte Tezlik";
 
         $resp = array('to' => array($email), 'pdf' => null, 'subject' => 'Nuevo Password', 'body' => $msg, 'ccHeader' => null);
         return $resp;
