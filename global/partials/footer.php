73057032838c482918fef299f61d755cd68f0738
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-08-18 19:07:27 -0500

    add version

--- global/partials/footer.php
+++ global/partials/footer.php
@@ -2,10 +2,10 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6 mb-1 mb-md-0">
-                <span>2022 &copy; Teenus SAS.</span>
+                <span>2023 &copy; Teenus SAS.</span>
             </div>
             <div class="col-md-6 text-md-right">
-                <span>Desarrollado por <span class="text-primary font-weight-500">Teenus SAS</span></span>
+                <span>Desarrollado por <span class="text-primary font-weight-500">Teenus SAS</span></span> v2.25.96
             </div>
         </div>
     </div>
