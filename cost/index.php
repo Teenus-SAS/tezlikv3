2a08ca7d418bf28fefeecfd43132c2cd2bfbc56b
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-09-18 13:27:27 -0500

    change colors border cards indicators

--- cost/index.php
+++ cost/index.php
@@ -82,7 +82,7 @@ if (sizeof($_SESSION) == 0)
                                 </div>
                             </div>
                             <div class="col">
-                                <div class="card radius-10 border-start border-0 border-3 border-info">
+                                <div class="card radius-10 border-start border-0 border-3 border-danger">
                                     <div class="card-body">
                                         <div class="media align-items-center">
                                             <div class="media-body">
@@ -100,7 +100,7 @@ if (sizeof($_SESSION) == 0)
                                 </div>
                             </div>
                             <div class="col">
-                                <div class="card radius-10 border-start border-0 border-3 border-info">
+                                <div class="card radius-10 border-start border-0 border-3 border-success">
                                     <div class="card-body">
                                         <div class="media align-items-center">
                                             <div class="media-body">
@@ -118,7 +118,7 @@ if (sizeof($_SESSION) == 0)
                                 </div>
                             </div>
                             <div class="col">
-                                <div class="card radius-10 border-start border-0 border-3 border-info">
+                                <div class="card radius-10 border-start border-0 border-3 border-warning">
                                     <div class="card-body">
                                         <div class="media align-items-center">
                                             <div class="media-body">
