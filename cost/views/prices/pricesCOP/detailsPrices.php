dcf454cbbbb6a394c223d32414569e462af24bbc
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-09-19 07:35:18 -0500

    sort object prices

--- cost/views/prices/pricesCOP/detailsPrices.php
+++ cost/views/prices/pricesCOP/detailsPrices.php
@@ -149,14 +149,14 @@ if (sizeof($_SESSION) == 0)
                                 <div class="card radius-10 border-start border-0 border-3 border-success">
                                     <div class="card-body">
                                         <div class="media align-items-center">
-                                            <div class="card-body">
+                                            <div class="card-body row row-cols-1 row-cols-md-2 row-cols-xl-2">
                                                 <div class="media-body align-items-center">
                                                     <span class="text-muted text-uppercase font-size-12 font-weight-bold">Precio de Venta Actual</span>
                                                     <h2 class="mb-0 mt-1 text-success" id="actualSalePrice" style="font-size: 19px"></h2>
-                                                    <div class="text-center">
-                                                        <span class="text-muted text-uppercase font-size-12 font-weight-bold">Precio de Venta Sugerido</span>
-                                                        <h3 class="mb-0 mt-1 text-warning suggestedPrice" style="font-size: 19px"></h3>
-                                                    </div>
+                                                </div>
+                                                <div class="media-body align-items-center">
+                                                    <span class="text-muted text-uppercase font-size-12 font-weight-bold">Precio de Venta Sugerido</span>
+                                                    <h3 class="mb-0 mt-1 text-warning suggestedPrice" style="font-size: 19px"></h3>
                                                 </div>
                                             </div>
                                         </div>
