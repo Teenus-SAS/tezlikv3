cac6f8adead841e3822bd21342ad994ae752318e
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-10-12 19:46:31 -0500

    add column

--- cost/views/perfil/perfil.php
+++ cost/views/perfil/perfil.php
@@ -108,8 +108,6 @@ if (sizeof($_SESSION) == 0)
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row">
                             <div class="col-lg-3">
                                 <div class="card">
                                     <div class="card-body text-center">
@@ -165,7 +163,6 @@ if (sizeof($_SESSION) == 0)
 
                         </div>
                     </form>
-
                 </div>
             </div>
         </div>
