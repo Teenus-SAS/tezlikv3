88c737d496899b60f9eeb3d81af5f8c4bf8deea6
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-10-12 20:14:07 -0500

    delete info irrelevant

--- planning/views/program/programming/programming.php
+++ planning/views/program/programming/programming.php
@@ -13,9 +13,9 @@ if (sizeof($_SESSION) == 0)
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="description" content="LetStart Admin is a full featured, multipurpose, premium bootstrap admin template built with Bootstrap 4 Framework, HTML5, CSS and JQuery.">
-    <meta name="keywords" content="admin, panels, dashboard, admin panel, multipurpose, bootstrap, bootstrap4, all type of dashboards">
-    <meta name="author" content="MatrrDigital">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Tezlik - Planning | Programming</title>
     <link rel="shortcut icon" href="/assets/images/favicon/favicon_tezlik.jpg" type="image/x-icon" />
