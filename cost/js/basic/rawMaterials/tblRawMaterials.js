4001192d08f73916ae1762f872d39c2d92eb0dae
Daniel Ledesma <daniel.ledesma@teenus.com.co>
2023-10-13 08:29:07 -0500

    Ajustes materia prima

--- cost/js/basic/rawMaterials/tblRawMaterials.js
+++ cost/js/basic/rawMaterials/tblRawMaterials.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
         render: (data, type, row) => {
           data == '' || !data
             ? (txt = '')
-            : (txt = `<img src="${data}" alt="" style="width:50px;border-radius:100px">`);
+            : (txt = `<img data-enlargable class="img" src="${data}" alt="" style="width:50px;border-radius:100px">`);
           return txt;
         },
       }, 
@@ -84,4 +84,20 @@ $(document).ready(function () {
       },
     ],
   });
+
+  // $('img[data-enlargable]').addClass('img-enlargable').click(function(){
+  $(document).on('click','.img', function () {
+    var src = $(this).attr('src');
+    $('<div>').css({
+        background: 'RGBA(0,0,0,.5) url('+src+') no-repeat center',
+        backgroundSize: 'contain',
+        width:'100%', height:'100%',
+        position:'fixed',
+        zIndex:'10000',
+        top:'0', left:'0',
+        cursor: 'zoom-out'
+    }).click(function(){
+        $(this).remove();
+    }).appendTo('body');
+});
 });
