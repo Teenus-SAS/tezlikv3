a4e6dd24ab3a5f0d0466c3582e566fdb1f053e98
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-10-16 19:41:01 -0500

    add change

--- cost/js/config/productProcess/tblConfigProcess.js
+++ cost/js/config/productProcess/tblConfigProcess.js
@@ -91,21 +91,21 @@ $(document).ready(function () {
             return `$ ${parseFloat(data).toLocaleString('es-co',{maximumFractionDigits: 2})}`;
           },
         },
-        /* {
+        {
           title: '',
           data: 'id_product_process',
           className: 'uniqueClassName',
           render: function (data) {
             return `<a href="javascript:;" <i id="${data}" class="ti-exchange-vertical updateEmployee" data-toggle='tooltip' title='Modificar Empleados' style="font-size: 30px; color:orange;"></i></a>`;
           },
-        }, */
+        },
         {
           title: 'Acciones',
           data: 'id_product_process',
           className: 'uniqueClassName',
           render: function (data) {
             return `
-            <a href="javascript:;" <i id="${data}" class="ti-exchange-vertical updateEmployee" data-toggle='tooltip' title='Modificar Empleados' style="font-size: 30px; color:orange;"></i></a>
+            
                 <a href="javascript:;" <i id="${data}" class="bx bx-edit-alt updateProcess" data-toggle='tooltip' title='Actualizar Proceso' style="font-size: 30px;"></i></a>
                 <a href="javascript:;" <i id="${data}" class="mdi mdi-delete-forever" data-toggle='tooltip' title='Eliminar Proceso' style="font-size: 30px;color:red" onclick="deleteFunction()"></i></a>`;
           },
