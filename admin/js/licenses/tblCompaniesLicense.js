af35fb8b7271089f92cc677b2feb4f00702607e8
Daniel Ledesma <daniel.ledesma@teenus.com.co>
2023-10-19 08:07:20 -0500

    Ajustes procesos nomina

--- admin/js/licenses/tblCompaniesLicense.js
+++ admin/js/licenses/tblCompaniesLicense.js
@@ -82,6 +82,16 @@ $(document).ready(function () {
         },
       },
       {
+        title: 'Acceso Procesos Nomina',
+        data: 'flag_employee',
+        className: 'uniqueClassName',
+        render: function (data) {
+          return data == 1
+            ? '<i class="bx bx-check text-success fs-lg align-middle"></i>'
+            : '<i class="bx bx-x text-danger fs-lg align-middle"></i>';
+        },
+      },
+      {
         title: 'Acciones',
         data: 'id_company',
         className: 'uniqueClassName',
