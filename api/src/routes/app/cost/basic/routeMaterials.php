216a93d6233d74fa9929e67e3ad6aa273b9f555e
Daniel Ledesma <daniel.ledesma@teenus.com.co>
2023-11-01 11:08:23 -0500

    Ajustes importe

--- api/src/routes/app/cost/basic/routeMaterials.php
+++ api/src/routes/app/cost/basic/routeMaterials.php
@@ -100,6 +100,8 @@ $app->post('/materialsDataValidation', function (Request $request, Response $res
 
         if (sizeof($dataImportMaterial) == 0) {
             for ($i = 0; $i < sizeof($materials); $i++) {
+                $materials[$i]['costRawMaterial'] = str_replace(',', '.', $materials[$i]['costRawMaterial']);
+
                 if (floatval($materials[$i]['costRawMaterial']) == false) {
                     $i = $i + 2;
                     $dataImportMaterial = array('error' => true, 'message' => "El costo debe ser mayor a cero (0), fila: $i");
