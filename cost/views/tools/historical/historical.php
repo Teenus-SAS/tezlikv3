83ff37391c5a62e4562b7ff5c9a869de6b9c4cf8
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-12-19 12:28:46 -0500

    add card historical

--- cost/views/tools/historical/historical.php
+++ cost/views/tools/historical/historical.php
@@ -43,7 +43,7 @@ if (sizeof($_SESSION) == 0)
             <!-- Content -->
             <div class="page-content">
                 <!-- Page header -->
-                <div class="page-title-box">
+                <div class="page-title-box" style="padding-bottom: 45px;">
                     <div class="container-fluid">
                         <div class="row align-items-center">
                             <div class="col-sm-5 col-xl-7">
@@ -54,54 +54,104 @@ if (sizeof($_SESSION) == 0)
 									</ol> -->
                                 </div>
                             </div>
-                            <div class="col-sm-5 col-xl-5 form-inline">
-                                <div class="col-sm-3 mb-1 d-flex align-items-center floating-label enable-floating-label show-label">
-                                    <label class="font-weight-bold text-dark">Mes</label>
-                                    <select id="month" class="form-control">
-                                        <option disabled selected>Seleccionar</option>
-                                        <option value="0">Todo</option>
-                                        <option value="1">Enero</option>
-                                        <option value="2">Febrero</option>
-                                        <option value="3">Marzo</option>
-                                        <option value="4">Abril</option>
-                                        <option value="5">Mayo</option>
-                                        <option value="6">Junio</option>
-                                        <option value="7">Julio</option>
-                                        <option value="8">Agosto</option>
-                                        <option value="9">Septiembre</option>
-                                        <option value="10">Octubre</option>
-                                        <option value="11">Noviembre</option>
-                                        <option value="12">Diciembre</option>
-                                    </select>
+                            <div class="col-sm-7 col-xl-5">
+                                <div class="row">
+                                    <div class="col-md-6 col-xl-4" style="padding-right: 0px;">
+                                        <div class="card bg-success">
+                                            <div class="card-body" style="padding: 10px;">
+                                                <div class="media text-white">
+                                                    <div class="media-body" style="text-align: center;">
+                                                        <span class="text-uppercase font-size-12 font-weight-bold" style="font-size: smaller;"><i class="bi bi-calendar-event mr-1"></i> Total: 310</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 col-xl-4" style="padding-right: 0px;">
+                                        <div class="card bg-warning">
+                                            <div class="card-body" style="padding: 10px;">
+                                                <div class="media text-white">
+                                                    <div class="media-body" style="text-align: center;">
+                                                        <span class="text-uppercase font-size-12 font-weight-bold" style="font-size: smaller;"><i class="bi bi-file-earmark-x mr-1"></i>Completados: 95%</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6 col-xl-4" style="padding-right: 0px;">
+                                        <div class="card bg-danger">
+                                            <div class="card-body" style="padding: 10px;">
+                                                <div class="media text-white">
+                                                    <div class="media-body" style="text-align: center;">
+                                                        <span class="text-uppercase font-size-12 font-weight-bold" style="font-size: smaller;"><i class="fas fa-arrow-down mr-1"></i>Atrasados: 5%</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="col-sm-3 mb-1 d-flex align-items-center floating-label enable-floating-label show-label">
-                                    <label class="font-weight-bold text-dark">Año</label>
-                                    <select id="year" class="form-control">
-                                        <option disabled selected>Seleccionar</option>
-                                        <option value="0">Todo</option>
-                                        <option value="2023">2023</option>
-                                        <option value="2022">2022</option>
-                                        <option value="2021">2021</option>
-                                        <option value="2020">2020</option>
-                                        <option value="2019">2019</option>
-                                    </select>
-                                </div>
-                                <div class="col-sm-6 mb-1 d-flex align-items-center floating-label enable-floating-label show-label">
-                                    <button class="btn btn-warning  mr-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Lista"><i class="fas fa-list-ul"></i></button>
-                                    <button class="btn btn-primary mr-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Graficos"><i class="fas fa-chart-line"></i></button>
-                                    <button class="btn btn-success" data-bs-toggle="tooltip" data-bs-placement="top" title="Si no ha guardado la información de costos del mes actual puede hacerlo dando clik aquí" id="btnSaveManualHistorical" name="btnSaveManualHistorical" class="btn btn-secondary"><i class="fas fa-file mr-1"></i>Guardar Hist</button>
-                                    <!-- <label>Tipo</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="page-content-wrapper mt--45" style="margin-bottom: 60px;">
+                    <div class="container-fluid">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row align-items-center justify-content-sm-end">
+                                    <div class="col-sm-5 col-xl-5 form-inline">
+                                        <div class="col-sm-3 mb-1 d-flex align-items-center floating-label enable-floating-label show-label">
+                                            <label class="font-weight-bold text-dark">Mes</label>
+                                            <select id="month" class="form-control">
+                                                <option disabled selected>Seleccionar</option>
+                                                <option value="0">Todo</option>
+                                                <option value="1">Enero</option>
+                                                <option value="2">Febrero</option>
+                                                <option value="3">Marzo</option>
+                                                <option value="4">Abril</option>
+                                                <option value="5">Mayo</option>
+                                                <option value="6">Junio</option>
+                                                <option value="7">Julio</option>
+                                                <option value="8">Agosto</option>
+                                                <option value="9">Septiembre</option>
+                                                <option value="10">Octubre</option>
+                                                <option value="11">Noviembre</option>
+                                                <option value="12">Diciembre</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-sm-3 mb-1 d-flex align-items-center floating-label enable-floating-label show-label">
+                                            <label class="font-weight-bold text-dark">Año</label>
+                                            <select id="year" class="form-control">
+                                                <option disabled selected>Seleccionar</option>
+                                                <option value="0">Todo</option>
+                                                <option value="2023">2023</option>
+                                                <option value="2022">2022</option>
+                                                <option value="2021">2021</option>
+                                                <option value="2020">2020</option>
+                                                <option value="2019">2019</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-sm-6 mb-1 d-flex align-items-center floating-label enable-floating-label show-label">
+                                            <button class="btn btn-warning  mr-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Lista"><i class="fas fa-list-ul"></i></button>
+                                            <button class="btn btn-primary mr-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Graficos"><i class="fas fa-chart-line"></i></button>
+                                            <button class="btn btn-success" data-bs-toggle="tooltip" data-bs-placement="top" title="Si no ha guardado la información de costos del mes actual puede hacerlo dando clik aquí" id="btnSaveManualHistorical" name="btnSaveManualHistorical" class="btn btn-secondary"><i class="fas fa-file mr-1"></i>Guardar Hist</button>
+                                            <!-- <label>Tipo</label>
                                         <select id="typeHistorical" class="form-control">
                                             <option disabled selected>Seleccionar</option>
                                             <option value="1">Tabla</option>
                                             <option value="2">Grafico</option>
                                         </select> -->
-                                </div>
+                                        </div>
 
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
 
                 <!-- page content -->
                 <div class="page-content-wrapper mt--45 cardTblPrices">
