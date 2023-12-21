4a86eaf456dd0dc9b25aa65d0e9e4b65713c72ff
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-12-21 10:29:01 -0500

    change view hist

--- cost/views/tools/historical/historical.php
+++ cost/views/tools/historical/historical.php
@@ -54,73 +54,141 @@ if (sizeof($_SESSION) == 0)
 									</ol> -->
                                 </div>
                             </div>
-                            <div class="col-sm-5 col-xl-5 form-inline justify-content-sm-end">
-                                <div class="col-sm-3 mb-4 d-flex align-items-center floating-label enable-floating-label show-label">
-                                    <label class="font-weight-bold text-dark">Mes</label>
-                                    <select id="month" class="form-control">
-                                        <option disabled selected>Seleccionar</option>
-                                        <option value="0">Todo</option>
-                                        <option value="1">Enero</option>
-                                        <option value="2">Febrero</option>
-                                        <option value="3">Marzo</option>
-                                        <option value="4">Abril</option>
-                                        <option value="5">Mayo</option>
-                                        <option value="6">Junio</option>
-                                        <option value="7">Julio</option>
-                                        <option value="8">Agosto</option>
-                                        <option value="9">Septiembre</option>
-                                        <option value="10">Octubre</option>
-                                        <option value="11">Noviembre</option>
-                                        <option value="12">Diciembre</option>
-                                    </select>
+                            <div class="col-sm-7 col-xl-5">
+                                <div class="row">
+                                    <div class="col-md-6 col-xl-4" style="padding-right: 0px;">
+                                        <div class="card bg-success shadow-lg">
+                                            <div class="card-body" style="padding: 10px;">
+                                                <div class="media text-white">
+                                                    <div class="media-body" style="text-align: center;">
+                                                        <span class="text-uppercase font-size-12 font-weight-bold" style="font-size: smaller;"><i class="fas fa-arrow-circle-up mr-1"></i> Rentab +Alta: 15%</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 col-xl-4" style="padding-right: 0px;">
+                                        <div class="card bg-danger shadow-lg">
+                                            <div class="card-body" style="padding: 10px;">
+                                                <div class="media text-white">
+                                                    <div class="media-body" style="text-align: center;">
+                                                        <span class="text-uppercase font-size-12 font-weight-bold" style="font-size: smaller;"><i class="fas fa-arrow-circle-down mr-1"></i>Rentab +Baja: -9%</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6 col-xl-4" style="padding-right: 0px;">
+                                        <div class="card bg-warning shadow-lg">
+                                            <div class="card-body" style="padding: 10px;">
+                                                <div class="media text-white">
+                                                    <div class="media-body" style="text-align: center;">
+                                                        <span class="text-uppercase font-size-12 font-weight-bold" style="font-size: smaller;"><i class="fas fa-arrow-circle-right mr-1"></i>Rentab Prom: 15%</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="col-sm-3 mb-4 d-flex align-items-center floating-label enable-floating-label show-label">
-                                    <label class="font-weight-bold text-dark">Año</label>
-                                    <select id="year" class="form-control">
-                                        <option disabled selected>Seleccionar</option>
-                                        <option value="0">Todo</option>
-                                        <option value="2023">2023</option>
-                                        <option value="2022">2022</option>
-                                        <option value="2021">2021</option>
-                                        <option value="2020">2020</option>
-                                        <option value="2019">2019</option>
-                                    </select>
-                                </div>
-                                <div class="col-sm-3 mb-4 d-flex align-items-center floating-label enable-floating-label show-label">
-                                    <!-- <button class="btn btn-warning" id="btnSaveManualHistorical" name="btnSaveManualHistorical">Guardar Historico</button> -->
-                                    <button class="btn btn-success typeHistorical mr-1" id="btnList"><i class="fas fa-list-ul"></i></button>
-                                    <button class="btn btn-primary typeHistorical mr-1" id="btnGraphic"><i class="fas fa-chart-line"></i></button>
-                                    <!-- <label>Tipo</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="page-content-wrapper mt--45" style="margin-bottom: 60px;">
+                    <div class="container-fluid">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row align-items-center justify-content-sm-end">
+                                    <div class="col-sm-5 col-xl-5 form-inline">
+                                        <div class="mb-1 mr-3 d-flex align-items-center floating-label enable-floating-label show-label">
+                                            <label class="font-weight-bold text-dark">Mes</label>
+                                            <select id="month" class="form-control">
+                                                <option disabled selected>Seleccionar</option>
+                                                <option value="0">Todo</option>
+                                                <option value="1">Enero</option>
+                                                <option value="2">Febrero</option>
+                                                <option value="3">Marzo</option>
+                                                <option value="4">Abril</option>
+                                                <option value="5">Mayo</option>
+                                                <option value="6">Junio</option>
+                                                <option value="7">Julio</option>
+                                                <option value="8">Agosto</option>
+                                                <option value="9">Septiembre</option>
+                                                <option value="10">Octubre</option>
+                                                <option value="11">Noviembre</option>
+                                                <option value="12">Diciembre</option>
+                                            </select>
+                                        </div>
+                                        <div class="mb-1 mr-3 d-flex align-items-center floating-label enable-floating-label show-label">
+                                            <label class="font-weight-bold text-dark">Año</label>
+                                            <select id="year" class="form-control">
+                                                <option disabled selected>Seleccionar</option>
+                                                <option value="0">Todo</option>
+                                                <option value="2023">2023</option>
+                                                <option value="2022">2022</option>
+                                                <option value="2021">2021</option>
+                                                <option value="2020">2020</option>
+                                                <option value="2019">2019</option>
+                                            </select>
+                                        </div>
+                                        <div class="mb-1 d-flex align-items-center floating-label enable-floating-label show-label">
+                                            <button class="btn btn-warning mr-1 shadow-lg" data-bs-toggle="tooltip" data-bs-placement="top" title="Lista"><i class="fas fa-list-ul"></i></button>
+                                            <button class="btn btn-success mr-1 shadow-lg" data-bs-toggle="tooltip" data-bs-placement="top" title="Graficos"><i class="fas fa-chart-line"></i></button>
+                                            <p style="font-size:40px; margin-bottom:0px; line-height:0px">|</p>
+                                            <button class="btn btn-primary ml-1 shadow-lg" data-bs-toggle="tooltip" data-bs-placement="top" title="Si no ha guardado la información de costos puede hacerlo dando clik aquí" id="btnSaveManualHistorical" name="btnSaveManualHistorical" class="btn btn-secondary"><i class="fas fa-save"></i></button>
+                                            <!-- <label>Tipo</label>
                                         <select id="typeHistorical" class="form-control">
                                             <option disabled selected>Seleccionar</option>
                                             <option value="1">Tabla</option>
                                             <option value="2">Grafico</option>
                                         </select> -->
-                                </div>
+                                        </div>
 
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
 
 
-        <!-- page content -->
-        <div class="page-content-wrapper mt--45 cardTblPrices">
-            <div class="container-fluid">
-                <!-- Row 5 -->
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card disable-select">
-                            <!-- <div class="card-header">
+                <!-- page content -->
+                <div class="page-content-wrapper mt--45 cardTblPrices">
+                    <div class="container-fluid">
+                        <!-- Row 5 -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card disable-select">
+                                    <!-- <div class="card-header">
                                         <h5 class="card-title">Precios</h5>
                                     </div> -->
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table class="table table-striped" id="tblHistorical">
+                                    <div class="card-body">
+                                        <div class="table-responsive">
+                                            <table class="table table-striped" id="tblHistorical">
 
-                                    </table>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="page-content-wrapper mt--45 cardDashboard">
+                    <div class="container-fluid">
+                        <!-- Row 5 -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card disable-select">
+                                    <div class="card-header">
+                                        <h5 class="card-title">Dashboard</h5>
+                                    </div>
+                                    <div class="card-body">
+
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -128,30 +196,10 @@ if (sizeof($_SESSION) == 0)
                 </div>
             </div>
         </div>
+        <!-- Main content end -->
 
-        <div class="page-content-wrapper mt--45 cardDashboard">
-            <div class="container-fluid">
-                <!-- Row 5 -->
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card disable-select">
-                            <div class="card-header">
-                                <h5 class="card-title">Dashboard</h5>
-                            </div>
-                            <div class="card-body">
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    </div>
-    <!-- Main content end -->
-
-    <!-- Footer -->
-    <?php include_once dirname(dirname(dirname(dirname(__DIR__)))) . '/global/partials/footer.php'; ?>
+        <!-- Footer -->
+        <?php include_once dirname(dirname(dirname(dirname(__DIR__)))) . '/global/partials/footer.php'; ?>
     </div>
     <!-- Page End -->
 
