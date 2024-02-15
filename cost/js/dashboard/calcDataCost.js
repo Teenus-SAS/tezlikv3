d71793039911d4b9b8e0cdf882704ad5d1dc5b84
Daniel Ledesma <daniel.ledesma@teenus.com.co>
2024-02-15 13:02:54 -0500

    change

--- cost/js/dashboard/calcDataCost.js
+++ cost/js/dashboard/calcDataCost.js
@@ -21,20 +21,21 @@ $(document).ready(function () {
 
     costCommissionSale = price * (data.commission_sale / 100);
 
-    // if (data.units_sold == 0 || data.turnover == 0) {
-    //   // profitability = (((data.sale_price * (1 - (data.commission_sale / 100))) - costTotal) / data.sale_price) * 100;
-    //   profitability = (((data.sale_price - costTotal) / costTotal) * 100);
+    // if (flag_expense == 2) {
+      // // profitability = (((data.sale_price * (1 - (data.commission_sale / 100))) - costTotal) / data.sale_price) * 100;
+      // profitability = (((data.sale_price - costTotal) / costTotal) * 100);
     // }
     // else {
       // profitability = ((data.sale_price - costTotal) / data.sale_price) * 100;
-    price2 = data.turnover / data.units_sold;
-    profitability = (((price2 - costTotal) / costTotal) * data.units_sold);
+      price2 = data.turnover / data.units_sold;
+      profitability = (((price2 - costTotal) / costTotal) * data.units_sold);
     // }
     profitability2 = (((data.sale_price - costTotal) / costTotal) * 100);
 
     costActualProfitability = data.sale_price * (profitability / 100);
 
     isNaN(profitability) ? profitability = 0 : profitability;
+    isNaN(profitability2) ? profitability2 = 0 : profitability2;
     isNaN(costProfitability) ? costProfitability = 0 : costProfitability;
     isNaN(costCommissionSale) ? costCommissionSale = 0 : costCommissionSale;
     isNaN(costActualProfitability) ? costActualProfitability = 0 : costActualProfitability;
