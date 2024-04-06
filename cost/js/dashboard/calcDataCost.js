406efd3ca8bb9ea33dc4108f83fd87e7b820642d
Daniel Ledesma <daniel.ledesma@teenus.com.co>
2024-04-06 11:53:46 -0500

    Ajustes modulos

--- cost/js/dashboard/calcDataCost.js
+++ cost/js/dashboard/calcDataCost.js
@@ -10,34 +10,34 @@ $(document).ready(function () {
     let cost_workforce = 0;
     let cost_indirect_cost = 0;
     let services = 0;
-    let expense = 0;
+    let expense = data.expense;
     let sale_price = 0;
     let turnover = 0;
 
-    let typePrice = sessionStorage.getItem('typePrice');
+    // let typePrice = sessionStorage.getItem('typePrice');
     
-    if (typePrice == '1' || !typePrice) {
-      cost_materials = parseFloat(data.cost_materials);
-      cost_workforce = parseFloat(data.cost_workforce);
-      cost_indirect_cost = parseFloat(data.cost_indirect_cost);
-      services = parseFloat(data.services);
-      expense = parseFloat(data.assignable_expense);
-      sale_price = parseFloat(data.sale_price);
-      turnover = parseFloat(data.turnover);
-    } else {
-      cost_materials = (parseFloat(data.cost_materials) / parseFloat(coverage));
-      cost_workforce = (parseFloat(data.cost_workforce) / parseFloat(coverage));
-      cost_indirect_cost = (parseFloat(data.cost_indirect_cost) / parseFloat(coverage));
-      services = (parseFloat(data.services) / parseFloat(coverage));
-      expense = (parseFloat(data.assignable_expense) / parseFloat(coverage));
-      sale_price = parseFloat(data.sale_price_usd);
-      turnover = (parseFloat(data.turnover) / parseFloat(coverage));
-    }
+    // if (typePrice == '1' || !typePrice) {
+    //   cost_materials = parseFloat(data.cost_materials);
+    //   cost_workforce = parseFloat(data.cost_workforce);
+    //   cost_indirect_cost = parseFloat(data.cost_indirect_cost);
+    //   services = parseFloat(data.services);
+    //   expense = parseFloat(data.assignable_expense);
+    //   sale_price = parseFloat(data.sale_price);
+    //   turnover = parseFloat(data.turnover);
+    // } else {
+    //   cost_materials = (parseFloat(data.cost_materials) / parseFloat(coverage));
+    //   cost_workforce = (parseFloat(data.cost_workforce) / parseFloat(coverage));
+    //   cost_indirect_cost = (parseFloat(data.cost_indirect_cost) / parseFloat(coverage));
+    //   services = (parseFloat(data.services) / parseFloat(coverage));
+    //   expense = (parseFloat(data.assignable_expense) / parseFloat(coverage));
+    //   sale_price = parseFloat(data.sale_price_usd);
+    //   turnover = (parseFloat(data.turnover) / parseFloat(coverage));
+    // }
 
-    cost = cost_materials + cost_workforce + cost_indirect_cost + services;
+    cost = data.cost_materials + data.cost_workforce + data.cost_indirect_cost + data.services;
 
     if (flag_expense == 0 || flag_expense == 1) {
-      costTotal = cost + expense;
+      costTotal = cost + data.expense;
     } else if (flag_expense == 2) {
       costTotal = cost / (1 - parseFloat(data.expense_recover) / 100);
       expense = costTotal * (parseFloat(data.expense_recover) / 100);
@@ -49,7 +49,7 @@ $(document).ready(function () {
     costProfitability = pPrice * (parseFloat(data.profitability) / 100);
 
     costCommissionSale = price * (parseFloat(data.commission_sale) / 100);
-    let recomendedPrice = turnover / parseFloat(data.units_sold);
+    let recomendedPrice = data.turnover / parseFloat(data.units_sold);
 
     if (flag_expense === "2" || flag_expense_distribution === "2") {
       price2 = 0;
@@ -58,14 +58,14 @@ $(document).ready(function () {
       // profitability2 = ((data.sale_price - costTotal) / data.sale_price) * 100;
       profitability = ((recomendedPrice - costTotal) / recomendedPrice) * 100;
       profitability2 = ((recomendedPrice - costTotal) / recomendedPrice) * 100;
-      costActualProfitability = parseFloat(sale_price) * (profitability / 100);
+      costActualProfitability = parseFloat(data.sale_price) * (profitability / 100);
 
     } else if ((data.units_sold > 0 && data.turnover > 0) || flag_expense === "1") {
       price2 = costTotal * parseFloat(data.units_sold);
-      profitability = ((turnover - price2) / price2) * 100;
+      profitability = ((data.turnover - price2) / price2) * 100;
       // profitability2 = (((parseFloat(data.sale_price) - costTotal) / parseFloat(data.sale_price)) * 100); 
       profitability2 = (((recomendedPrice - costTotal) / recomendedPrice) * 100);
-      costActualProfitability = turnover - price2;
+      costActualProfitability = data.turnover - price2;
     }
 
     isNaN(profitability) ? (profitability = 0) : profitability;
