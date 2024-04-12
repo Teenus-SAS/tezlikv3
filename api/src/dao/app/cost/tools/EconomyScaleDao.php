8f037b096aab0628d40f6bdad63d2aed22793fd4
Daniel Ledesma <daniel.ledesma@teenus.com.co>
2024-04-12 09:43:07 -0500

    change

--- api/src/dao/app/cost/tools/EconomyScaleDao.php
+++ api/src/dao/app/cost/tools/EconomyScaleDao.php
@@ -24,7 +24,7 @@ class EconomyScaleDao
             $stmt = $connection->prepare("SELECT p.id_product, IFNULL((IF(IFNULL(mp.units_sold , 0) = 0, (SELECT IFNULL(SUM(salary_net), 0) AS salary_net FROM (SELECT salary_net FROM payroll WHERE id_company = :id_company GROUP BY employee) AS payroll), ((SELECT IFNULL(SUM(salary_net), 0) AS salary_net FROM (SELECT salary_net FROM payroll WHERE id_company = :id_company GROUP BY employee) AS payroll) * (mp.participation / 100)))) , 0) + IFNULL((IF(IFNULL(mp.units_sold, 0) = 0,(SELECT SUM(e.expense_value) FROM expenses e INNER JOIN puc pu ON e.id_puc = pu.id_puc WHERE e.id_company = p.id_company),
                                              (SELECT SUM(e.expense_value) FROM expenses e INNER JOIN puc pu ON e.id_puc = pu.id_puc WHERE e.id_company = p.id_company) * (mp.participation / 100))), 0) AS costFixed
                                         FROM products p
-                                        LEFT JOIN products_costs pc ON pc.id_product = p.id_product
+                                        INNER JOIN products_costs pc ON pc.id_product = p.id_product
                                         LEFT JOIN multiproducts mp ON mp.id_product = p.id_product
                                       WHERE p.active = 1 AND p.id_company = :id_company");
             $stmt->execute([
@@ -46,7 +46,7 @@ class EconomyScaleDao
             $stmt = $connection->prepare("SELECT p.id_product, ((pc.commission_sale / 100) * IF(cl.flag_type_price = 0, pc.sale_price, pc.price)) AS commission, pc.cost_materials + pc.cost_indirect_cost + ((pc.commission_sale / 100) * pc.price) + 
                                              (SELECT IFNULL(SUM(cost), 0) FROM services WHERE id_product = p.id_product) AS variableCost
                                       FROM products p
-                                        LEFT JOIN products_costs pc ON pc.id_product = p.id_product
+                                        INNER JOIN products_costs pc ON pc.id_product = p.id_product
                                         LEFT JOIN companies_licenses cl ON cl.id_company = p.id_company
                                       WHERE p.active = 1 AND p.id_company = :id_company");
             $stmt->execute([
@@ -102,13 +102,25 @@ class EconomyScaleDao
 
     public function combinedData($array1, $array2, $key)
     {
-        $result = array();
-        foreach ($array1 as $item) {
-            $result[$item[$key]] = $item;
+        try {
+            $result = array();
+
+            // Crear un índice para $array1 basado en la clave
+            $index = array();
+            foreach ($array1 as $item) {
+                $index[$item[$key]] = $item;
+            }
+
+            // Iterar sobre $array2 y combinar solo si la clave existe en $array1
+            foreach ($array2 as $item) {
+                if (isset($index[$item[$key]])) {
+                    $result[$item[$key]] = array_merge($index[$item[$key]], $item);
+                }
+            }
+
+            return array_values($result);
+        } catch (\Exception $e) {
+            return $e->getMessage();
         }
-        foreach ($array2 as $item) {
-            $result[$item[$key]] = array_merge($result[$item[$key]], $item);
-        }
-        return array_values($result);
     }
 }
--- api/src/routes/app/cost/tools/routeEconomyScale.php
+++ api/src/routes/app/cost/tools/routeEconomyScale.php
@@ -11,28 +11,9 @@ $generalCompanyLicenseDao = new GeneralCompanyLicenseDao();
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-// $app->get('/calcEconomyScale/{id_product}', function (Request $request, Response $response, $args) use ($priceDao, $economyScaleDao) {
-//     session_start();
-//     $id_company = $_SESSION['id_company'];
-
-//     $price = $priceDao->findPriceByProduct($args['id_product'], $id_company);
-//     $fixedCosts = $economyScaleDao->findFixedCostByProduct($args['id_product'], $id_company);
-//     $variableCosts = $economyScaleDao->findVariableCostByProduct($args['id_product'], $id_company);
-
-//     $data['price'] = $price['price'];
-//     $data['sale_price'] = $price['sale_price'];
-//     $data['profitability'] = $price['profitability'];
-//     $data['fixedCost'] = $fixedCosts['costFixed'];
-//     $data['variableCost'] = $variableCosts['variableCost'];
-//     $data['commission'] = $variableCosts['commission'];
-
-//     $response->getBody()->write(json_encode($data, JSON_NUMERIC_CHECK));
-//     return $response->withHeader('Content-Type', 'application/json');
-// });
 $app->get('/calcEconomyScale', function (Request $request, Response $response, $args) use ($priceDao, $economyScaleDao) {
     session_start();
     $id_company = $_SESSION['id_company'];
-    // $id_company = 2;
 
     $price = $priceDao->findAllPricesByCompany($id_company);
     $fixedCosts = $economyScaleDao->findAllFixedCostByCompany($id_company);
