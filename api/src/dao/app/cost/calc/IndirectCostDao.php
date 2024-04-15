93b4a9477371247412c1508bac8fdead82344adc
Daniel Ledesma <daniel.ledesma@teenus.com.co>
2024-04-15 11:13:57 -0500

    change

--- api/src/dao/app/cost/calc/IndirectCostDao.php
+++ api/src/dao/app/cost/calc/IndirectCostDao.php
@@ -44,7 +44,7 @@ class IndirectCostDao
         $connection = Connection::getInstance()->getConnection();
 
         try {
-            $stmt = $connection->prepare("SELECT pp.id_product_process, pp.id_machine, m.minute_depreciation, pp.operation_time 
+            $stmt = $connection->prepare("SELECT pp.id_product_process, pp.id_machine, m.minute_depreciation, pp.enlistment_time, pp.operation_time, pp.efficiency 
                                       FROM products_process pp 
                                       INNER JOIN machines m ON m.id_machine = pp.id_machine 
                                       WHERE pp.id_product = :id_product AND pp.id_company = :id_company");
@@ -79,7 +79,8 @@ class IndirectCostDao
                 $dataCostManufacturingLoad = $stmt->fetch($connection::FETCH_ASSOC);
 
                 // Calculo costo indirecto
-                $processMachineindirectCost = ($dataCostManufacturingLoad['totalCostMinute'] + $dataProductMachine[$i]['minute_depreciation']) * $dataProductMachine[$i]['operation_time'];
+                // $processMachineindirectCost = ($dataCostManufacturingLoad['totalCostMinute'] + $dataProductMachine[$i]['minute_depreciation']) * $dataProductMachine[$i]['operation_time'];
+                $processMachineindirectCost = ($dataCostManufacturingLoad['totalCostMinute'] + $dataProductMachine[$i]['minute_depreciation']) * ($dataProductMachine[$i]['enlistment_time'] + $dataProductMachine[$i]['operation_time'] * ($dataProductMachine[$i]['efficiency'] / 100));
 
                 // Guardar Costo indirecto
                 $this->updateCostIndirectCost($processMachineindirectCost, $dataProductMachine[$i]['id_product_process']);
