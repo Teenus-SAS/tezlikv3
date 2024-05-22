db10452f27e26c530360c04a6f712a9861c0febd
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-05-22 13:39:29 -0500

    change icon btn

--- cost/views/config/productMaterials.php
+++ cost/views/config/productMaterials.php
@@ -93,7 +93,7 @@ if (sizeof($_SESSION) == 0)
                                         <button class="btn btn-info" id="btnImportNewProductsMaterials" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Importar Materia Prima"><i class="fas fa-download" style="font-size: 20px;"></i></button>
                                     </div>
                                     <div class="col-xs-2 py-2 mr-2">
-                                        <button class="btn btn-secondary btnDownloadXlsx"><i class="bi bi-cloud-arrow-up-fill"></i></button>
+                                        <button class="btn btn-secondary btnDownloadXlsx"><i class="fas fa-cloud-upload-alt" style="font-size: 20px;"></i></button>
                                     </div>
                                 </div>
                             </div>
