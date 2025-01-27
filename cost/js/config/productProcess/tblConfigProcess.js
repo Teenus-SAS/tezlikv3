daac375b864ba03fb83883c2ac07b0c09185fc89
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2025-01-26 21:15:22 -0500

    add change min to seg FTTP

--- cost/js/config/productProcess/tblConfigProcess.js
+++ cost/js/config/productProcess/tblConfigProcess.js
@@ -1,26 +1,59 @@
 $(document).ready(function () {
-  let title3 = `${inyection == 1 ? 'Tiempo/Und' : 'Tiempo Alistamiento (min)'}`;
-  let value3 = `${inyection == 1 ? 'unity_time' : 'enlistment_time'}`;
-  let title4 = `${inyection == 1 ? '% Eficiencia' : 'Tiempo Operación (min)'}`;
-  let visible; 
+  let title3 = `${inyection == 1 ? "Tiempo/Und" : "Tiempo Alistamiento (min)"}`;
+  let value3 = `${inyection == 1 ? "unity_time" : "enlistment_time"}`;
+  let title4 = `${inyection == 1 ? "% Eficiencia" : "Tiempo Operación (min)"}`;
+  let visible;
+
+  let isMinutes = true; // Variable para rastrear el estado actual (minutos o segundos)
+  const headerSelector =
+    '#tblConfigProcess thead th:contains("Tiempo Operación (min)")';
+  const bodySelector = "#tblConfigProcessBody tr";
+
+  // Función para convertir minutos a segundos
+  const convertToSeconds = (value) => (value * 60).toFixed(2);
+
+  // Función para convertir segundos a minutos
+  const convertToMinutes = (value) => (value / 60).toFixed(2);
+
+  // Evento al hacer clic en el encabezado
+  $(document).on("click", headerSelector, function () {
+    // Cambiar el texto del encabezado
+    const newText = isMinutes
+      ? "Tiempo Operación (seg)"
+      : "Tiempo Operación (min)";
+    $(this).text(newText);
+
+    // Cambiar los datos en las filas de la tabla
+    $(bodySelector).each(function () {
+      const cell = $(this).find("td").eq(5); // Índice de la columna "Tiempo Operación"
+      const currentValue = parseFloat(cell.text().replace(/,/g, "")); // Convertir a número
+      const newValue = isMinutes
+        ? convertToSeconds(currentValue)
+        : convertToMinutes(currentValue);
+      cell.text(newValue.toLocaleString("es-CO", { maximumFractionDigits: 2 }));
+    });
+
+    // Cambiar el estado de minutos a segundos y viceversa
+    isMinutes = !isMinutes;
+  });
 
   /* Seleccion producto */
-  $('#refProduct').change(function (e) {
+  $("#refProduct").change(function (e) {
     e.preventDefault();
     let id = this.value;
 
-    $('#selectNameProduct option').prop('selected', function () {
+    $("#selectNameProduct option").prop("selected", function () {
       return $(this).val() == id;
     });
 
     loadAllDataProcess(id);
   });
 
-  $('#selectNameProduct').change(function (e) {
+  $("#selectNameProduct").change(function (e) {
     e.preventDefault();
     let id = this.value;
 
-    $('#refProduct option').prop('selected', function () {
+    $("#refProduct option").prop("selected", function () {
       return $(this).val() == id;
     });
 
@@ -31,35 +64,38 @@ $(document).ready(function () {
     try {
       const productsProcess = await searchData(`/api/productsProcess/${id}`);
 
-      sessionStorage.setItem('dataProductProcess', JSON.stringify(productsProcess));
+      sessionStorage.setItem(
+        "dataProductProcess",
+        JSON.stringify(productsProcess)
+      );
 
       let op = 1;
-      if(flag_currency_usd == '1'){
-        let selectPriceUSD = $('#selectPriceUSD2').val();
+      if (flag_currency_usd == "1") {
+        let selectPriceUSD = $("#selectPriceUSD2").val();
 
-        selectPriceUSD == '2' ? op = 2 : op = 1;
+        selectPriceUSD == "2" ? (op = 2) : (op = 1);
       }
       // dataProductProcess = productsProcess;
 
       loadTableProcess(productsProcess, op);
     } catch (error) {
-      console.error('Error loading data:', error);
+      console.error("Error loading data:", error);
     }
   };
 
   // loadAllDataProcess(0);
 
-  flag_employee == '1' ? visible = true : visible = false;
+  flag_employee == "1" ? (visible = true) : (visible = false);
 
   /* Cargue tabla de Proyectos */
 
   loadTableProcess = (data, op) => {
-    $('.cardAddProcess').hide(800); 
-    
+    $(".cardAddProcess").hide(800);
+
     if ($.fn.dataTable.isDataTable("#tblConfigProcess")) {
       $("#tblConfigProcess").DataTable().destroy();
       $("#tblConfigProcess").empty();
-      $('#tblConfigProcess').append(`
+      $("#tblConfigProcess").append(`
         <tbody id="tblConfigProcessBody"></tbody>
         <tfoot>
           <tr>
@@ -79,56 +115,66 @@ $(document).ready(function () {
 
     let totalEfficiency = 0;
 
-    data.forEach(item => {
+    data.forEach((item) => {
       totalEfficiency += parseFloat(item.efficiency);
     });
- 
+
     // Encabezados de la tabla
-    var headers = ['No.', 'Proceso', 'Máquina', 'Operarios', title3, title4, 'Eficiencia', 'Mano de Obra', 'Costo Indirecto', 'Acciones'];
-    
-    if (totalEfficiency == 0)
-      headers.splice(6, 1);
-    
+    var headers = [
+      "No.",
+      "Proceso",
+      "Máquina",
+      "Operarios",
+      title3,
+      title4,
+      "Eficiencia",
+      "Mano de Obra",
+      "Costo Indirecto",
+      "Acciones",
+    ];
+
+    if (totalEfficiency == 0) headers.splice(6, 1);
+
     // Obtén la tabla
-    var table = document.getElementById('tblConfigProcess');
+    var table = document.getElementById("tblConfigProcess");
 
     // Crea la fila de encabezados
     var headerRow = table.createTHead().insertRow();
     headers.forEach(function (header) {
-      var th = document.createElement('th');
+      var th = document.createElement("th");
       th.textContent = header;
       headerRow.appendChild(th);
     });
 
-    $('#tblConfigProcessBody').empty();
-    var body = document.getElementById('tblConfigProcessBody');
+    $("#tblConfigProcessBody").empty();
+    var body = document.getElementById("tblConfigProcessBody");
 
     data.forEach((arr, index) => {
       const i = index;
       const dataRow = body.insertRow();
 
-      dataRow.classList.add('t-row'); // Agregar la clase 't-row' a la fila
-      dataRow.setAttribute('data-index', index);
-      dataRow.setAttribute('data-id', arr.id_product_process);
+      dataRow.classList.add("t-row"); // Agregar la clase 't-row' a la fila
+      dataRow.setAttribute("data-index", index);
+      dataRow.setAttribute("data-id", arr.id_product_process);
 
       headers.forEach((header, columnIndex) => {
         const cell = dataRow.insertCell();
 
         switch (header) {
-          case 'No.': 
+          case "No.":
             cell.textContent = i + 1;
             break;
-          case 'Proceso':
+          case "Proceso":
             cell.textContent = arr.process;
             break;
-          case 'Máquina':
+          case "Máquina":
             cell.textContent = arr.machine;
             break;
-          case 'Operarios':
+          case "Operarios":
             let count_employee = arr.count_employee;
 
             let employees = arr.employee.toString().split(",");
-            if (employees[0] != '') {
+            if (employees[0] != "") {
               count_employee = employees.length;
             }
 
@@ -136,110 +182,132 @@ $(document).ready(function () {
             break;
           case title3:
             let value = parseFloat(arr[value3]);
-            
+
             if (Math.abs(value) < 0.01) {
-              value = value.toLocaleString('es-CO', { minimumFractionDigits: 2, maximumFractionDigits: 9 });
+              value = value.toLocaleString("es-CO", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 9,
+              });
             } else
-              value = value.toLocaleString('es-CO', { maximumFractionDigits: 2 });
-            
+              value = value.toLocaleString("es-CO", {
+                maximumFractionDigits: 2,
+              });
+
             cell.textContent = value;
             break;
           case title4:
             let operation_time = parseFloat(arr.operation_time);
-            
-            if (Math.abs(operation_time) < 0.01) { 
-              operation_time = operation_time.toLocaleString('es-CO', { minimumFractionDigits: 2, maximumFractionDigits: 9 });
+
+            if (Math.abs(operation_time) < 0.01) {
+              operation_time = operation_time.toLocaleString("es-CO", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 9,
+              });
             } else
-              operation_time = operation_time.toLocaleString('es-CO', { maximumFractionDigits: 2 });
-            
+              operation_time = operation_time.toLocaleString("es-CO", {
+                maximumFractionDigits: 2,
+              });
+
             cell.textContent = operation_time;
             break;
-          case 'Eficiencia':
+          case "Eficiencia":
             let efficiency = parseFloat(arr.efficiency);
-            
-            if (Math.abs(efficiency) < 0.01) { 
-              efficiency = efficiency.toLocaleString('es-CO', { minimumFractionDigits: 2, maximumFractionDigits: 9 });
+
+            if (Math.abs(efficiency) < 0.01) {
+              efficiency = efficiency.toLocaleString("es-CO", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 9,
+              });
             } else
-              efficiency = efficiency.toLocaleString('es-CO', { maximumFractionDigits: 2 });
-            
+              efficiency = efficiency.toLocaleString("es-CO", {
+                maximumFractionDigits: 2,
+              });
+
             cell.textContent = `${efficiency} %`;
             break;
-          case 'Mano de Obra':
+          case "Mano de Obra":
             let workforce_cost = parseFloat(arr.workforce_cost);
 
-            if (flag_currency_usd == '1' && op == 2)
-              workforce_cost = workforce_cost / parseFloat(coverage_usd); 
+            if (flag_currency_usd == "1" && op == 2)
+              workforce_cost = workforce_cost / parseFloat(coverage_usd);
             workforce_cost = renderCost(workforce_cost, op);
-            
+
             cell.textContent = workforce_cost;
             break;
-          case 'Costo Indirecto':
+          case "Costo Indirecto":
             let indirect_cost = parseFloat(arr.indirect_cost);
 
-            if (flag_currency_usd == '1' && op == 2)
-              indirect_cost = indirect_cost / parseFloat(coverage_usd);   
-            indirect_cost = renderCost(indirect_cost, op); 
-            
+            if (flag_currency_usd == "1" && op == 2)
+              indirect_cost = indirect_cost / parseFloat(coverage_usd);
+            indirect_cost = renderCost(indirect_cost, op);
+
             cell.textContent = indirect_cost;
-            break; 
-          case 'Acciones':  
-            cell.innerHTML =
-               `<a href="javascript:;" <i id="${arr.id_product_process}" class="bx bx-edit-alt updateProcess" data-toggle='tooltip' title='Actualizar Proceso' style="font-size: 30px;"></i></a>
+            break;
+          case "Acciones":
+            cell.innerHTML = `<a href="javascript:;" <i id="${arr.id_product_process}" class="bx bx-edit-alt updateProcess" data-toggle='tooltip' title='Actualizar Proceso' style="font-size: 30px;"></i></a>
                 <a href="javascript:;" <i id="${arr.id_product_process}" class="mdi mdi-delete-forever" data-toggle='tooltip' title='Eliminar Proceso' style="font-size: 30px;color:red" onclick="deleteProcess(${arr.id_product_process})"></i></a>`;
             break;
           default:
-            cell.textContent = '';
+            cell.textContent = "";
             break;
         }
       });
     });
 
-    if (totalEfficiency == 0) $('#totalEfficiency').remove();
+    if (totalEfficiency == 0) $("#totalEfficiency").remove();
 
-    $('#tblConfigProcess').dataTable({
+    $("#tblConfigProcess").dataTable({
       pageLength: 50,
       dom: '<"datatable-error-console">frtip',
       language: {
-        url: '/assets/plugins/i18n/Spanish.json',
+        url: "/assets/plugins/i18n/Spanish.json",
       },
       fnInfoCallback: function (oSettings, iStart, iEnd, iMax, iTotal, sPre) {
-        if (oSettings.json && oSettings.json.hasOwnProperty('error')) {
+        if (oSettings.json && oSettings.json.hasOwnProperty("error")) {
           console.error(oSettings.json.error);
         }
       },
     });
 
-    dragula([document.getElementById('tblConfigProcessBody')]).on('drop', function (el, container, source, sibling) {
-      let copy = [];
+    dragula([document.getElementById("tblConfigProcessBody")]).on(
+      "drop",
+      function (el, container, source, sibling) {
+        let copy = [];
 
-      // If the row was dropped within the same container,
-      // move it to the specified position
-      if (container === source) {
-        var targetIndex = sibling ? sibling.rowIndex - 1 : container.children.length - 1;
-        
-        container.insertBefore(el, container.children[targetIndex]);
-        
-        var elements = $('.t-row');
-        elements = elements.not('.gu-mirror');
+        // If the row was dropped within the same container,
+        // move it to the specified position
+        if (container === source) {
+          var targetIndex = sibling
+            ? sibling.rowIndex - 1
+            : container.children.length - 1;
 
-        for (let i = 0; i < elements.length; i++) {
-          copy.push({ id_product_process: elements[i].dataset.id, route: i + 1 });
-        } 
+          container.insertBefore(el, container.children[targetIndex]);
 
-        $.ajax({
-          type: "POST",
-          url: "/api/saveRouteProductProcess",
-          data: { data: copy },
-          success: function (resp) {
-            messageProcess(resp);
+          var elements = $(".t-row");
+          elements = elements.not(".gu-mirror");
+
+          for (let i = 0; i < elements.length; i++) {
+            copy.push({
+              id_product_process: elements[i].dataset.id,
+              route: i + 1,
+            });
           }
-        });
-      } else {
-        // If the row was dropped into a different container,
-        // move it to the first position
-        container.insertBefore(el, container.firstChild);
+
+          $.ajax({
+            type: "POST",
+            url: "/api/saveRouteProductProcess",
+            data: { data: copy },
+            success: function (resp) {
+              messageProcess(resp);
+            },
+          });
+        } else {
+          // If the row was dropped into a different container,
+          // move it to the first position
+          container.insertBefore(el, container.firstChild);
+        }
       }
-    });
+    );
 
     let alistment = 0;
     let operation = 0;
@@ -247,12 +315,18 @@ $(document).ready(function () {
     let workForce = 0;
     let indirect = 0;
 
-    data.forEach(item => {
+    data.forEach((item) => {
       alistment += parseFloat(item[value3]);
       operation += parseFloat(item.operation_time);
       efficiency += parseFloat(item.efficiency);
-      workForce += flag_currency_usd == '1' && op == 2 ? parseFloat(item.workforce_cost) / parseFloat(coverage_usd) : parseFloat(item.workforce_cost);
-      indirect += flag_currency_usd == '1' && op == 2 ? parseFloat(item.indirect_cost) / parseFloat(coverage_usd) : parseFloat(item.indirect_cost);
+      workForce +=
+        flag_currency_usd == "1" && op == 2
+          ? parseFloat(item.workforce_cost) / parseFloat(coverage_usd)
+          : parseFloat(item.workforce_cost);
+      indirect +=
+        flag_currency_usd == "1" && op == 2
+          ? parseFloat(item.indirect_cost) / parseFloat(coverage_usd)
+          : parseFloat(item.indirect_cost);
       // indirect += parseFloat(item.indirect_cost);
     });
 
@@ -260,10 +334,16 @@ $(document).ready(function () {
     workForce = renderCost(workForce, op);
     indirect = renderCost(indirect, op);
 
-    $('#totalAlistment').html(alistment.toLocaleString('es-CO', { maximumFractionDigits: 2 }));
-    $('#totalOperation').html(operation.toLocaleString('es-CO', { maximumFractionDigits: 2 }));
-    $('#totalEfficiency').html(`${efficiency.toLocaleString('es-CO', { maximumFractionDigits: 2 })} %`);
-    $('#totalWorkforce').html(workForce);
-    $('#totalIndirect').html(indirect);
+    $("#totalAlistment").html(
+      alistment.toLocaleString("es-CO", { maximumFractionDigits: 2 })
+    );
+    $("#totalOperation").html(
+      operation.toLocaleString("es-CO", { maximumFractionDigits: 2 })
+    );
+    $("#totalEfficiency").html(
+      `${efficiency.toLocaleString("es-CO", { maximumFractionDigits: 2 })} %`
+    );
+    $("#totalWorkforce").html(workForce);
+    $("#totalIndirect").html(indirect);
   };
 });
